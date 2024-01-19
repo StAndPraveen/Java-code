d67a511f1ba49feae50119dcb865b4cba8e25f1d
praveen <preysentg@gmail.com>
2024-01-19 09:23:52 +0530

    commit

--- arrayprg.java
+++ arrayprg.java
@@ -9,7 +9,7 @@ int i,n;
 Scanner s= new  Scanner(System.in);
 
 System.out.println("How many values are there?");
-n=s.nextInt();
+n=s.nextInt(); 
 
 System.out.println("Enter the values :");
 for(i=0;i<n;i++) arr[i]=s.nextInt();
